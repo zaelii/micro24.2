a1577f17f3bd5237abc4b209d70801888e35354d
Zaeli <75710725+zaelii@users.noreply.github.com>
2024-11-21 17:53:17 -0300

    Update main.c

--- main.c
+++ main.c
@@ -228,6 +228,114 @@ void questao5(void){
     }
 }
 
+void questao6(void) {
+    Utility_Init();
+    GPIO_Clock_Enable(GPIOA);
+
+    // Configurando os pinos como saída
+    GPIO_Pin_Mode(GPIOA, PIN_0, OUTPUT); // Verde Semáforo 1
+    GPIO_Pin_Mode(GPIOA, PIN_1, OUTPUT); // Amarelo Semáforo 1
+    GPIO_Pin_Mode(GPIOA, PIN_2, OUTPUT); // Vermelho Semáforo 1
+    GPIO_Pin_Mode(GPIOA, PIN_3, OUTPUT); // Verde Semáforo 2
+    GPIO_Pin_Mode(GPIOA, PIN_4, OUTPUT); // Amarelo Semáforo 2
+    GPIO_Pin_Mode(GPIOA, PIN_5, OUTPUT); // Vermelho Semáforo 2
+
+    while (1) {
+        GPIO_Write_Pin(GPIOA, PIN_2, HIGH);
+        GPIO_Write_Pin(GPIOA, PIN_3, HIGH);
+        Delay_ms(5000);
+        GPIO_Write_Pin(GPIOA, PIN_2, LOW);
+
+        GPIO_Write_Pin(GPIOA, PIN_1, HIGH);
+        Delay_ms(2000);
+        GPIO_Write_Pin(GPIOA, PIN_1, LOW);
+
+        GPIO_Write_Pin(GPIOA, PIN_0, HIGH);
+        GPIO_Write_Pin(GPIOA, PIN_5, HIGH);
+        GPIO_Write_Pin(GPIOA, PIN_3, LOW);
+        Delay_ms(5000);
+        GPIO_Write_Pin(GPIOA, PIN_5, LOW);
+
+        GPIO_Write_Pin(GPIOA, PIN_4, HIGH);
+        Delay_ms(2000);
+        GPIO_Write_Pin(GPIOA, PIN_4, LOW);
+
+        GPIO_Write_Pin(GPIOA, PIN_3, HIGH);
+        GPIO_Write_Pin(GPIOA, PIN_0, LOW);
+    }
+}
+
+void questao7(void) {
+	// inicia o sistema de clock
+	Utility_Init();
+
+	// configura PA6 e PA7 como saida
+	GPIO_Clock_Enable(GPIOA);
+
+	GPIO_Pin_Mode(GPIOA, PIN_0, OUTPUT);
+	GPIO_Pin_Mode(GPIOA, PIN_1, OUTPUT);
+	GPIO_Pin_Mode(GPIOA, PIN_2, OUTPUT);
+	GPIO_Pin_Mode(GPIOA, PIN_3, OUTPUT);
+	GPIO_Pin_Mode(GPIOA, PIN_4, OUTPUT);
+	GPIO_Pin_Mode(GPIOA, PIN_5, OUTPUT);
+	GPIO_Pin_Mode(GPIOA, PIN_6, OUTPUT);
+
+	// Decodificação de display de 7 segmentos com uso de máscaras em matriz
+	const uint8_t mask[16]={ 0b00111111, //0
+	0b00000110, //1
+	0b01011011, //2
+	0b01001111, //3
+	0b01100110, //4
+	0b01101101, //5
+	0b01111101, //6
+	0b00000111, //7
+	0b01111111, //8
+	0b01101111, //9
+	0b01110111, //A
+	0b01111100, //B
+	0b00111001, //C
+	0b01011110, //D
+	0b01111001, //E
+	0b01110001 //F
+	};
+
+	while(1) {
+
+		for (int i = 0; i < 16; i++) {
+			GPIO_Write_Port(GPIOA, mask[i]);
+			Delay_ms(300);
+		}
+        for (int i = 15; i >= 0; i--) {
+        	GPIO_Write_Port(GPIOA, mask[i]);
+            Delay_ms(300);
+        }
+    }
+}
+
+void questao9(void) {
+	// inicia o sistema de clock
+	Utility_Init();
+
+	// configura PE4 como saida
+	GPIO_Clock_Enable(GPIOE);
+	GPIO_Pin_Mode(GPIOE, PIN_4, OUTPUT);
+
+	int tempo = 0;
+	int incremento = 50;
+
+	while(1){
+		GPIO_Write_Pin(GPIOE, PIN_4, HIGH);
+		Delay_us(tempo);
+		GPIO_Write_Pin(GPIOE, PIN_4, LOW);
+		Delay_us(20000 - tempo);
+		tempo = tempo + incremento;
+		if (tempo == 2500) incremento = -incremento;
+		if (tempo == 0) incremento = -incremento;
+	}
+
+
+
+}
 
 void questao13(void){
 	// inicia o sistema de clock
@@ -310,7 +418,7 @@ void questao14(void) {
     }
 }
 
-void questao15incompleto(void) {
+void questao15(void) {
 	// LIGANDO o clock do GPIOA(leds) e GPIOE(buttoes)
 	GPIO_Clock_Enable(GPIOA);
 	GPIO_Clock_Enable(GPIOE);
@@ -321,23 +429,51 @@ void questao15incompleto(void) {
 	GPIO_Pin_Mode(GPIOE, PIN_4, INPUT);
 
 	// LIGANDO os resistores de pull-up
-	GPIO_Resistor_Enable(GPIOE, PIN_3, PULL_UP);
-	GPIO_Resistor_Enable(GPIOE, PIN_4, PULL_UP);
+	GPIO_Resistor_Enable(GPIOE, PIN_3, PULL_UP); // k1
+	GPIO_Resistor_Enable(GPIOE, PIN_4, PULL_UP); // k0
 
-	// ESTADO Inicial dos leds
-	GPIO_Write_Pin(GPIOA, PIN_6, HIGH);
+
+
 
 
 	while(1){
-		if (GPIO_Read_Pin(GPIOE, PIN_3) == LOW){
+		int k0_apertado = 0;
+		int k1_apertado = 0;
+		// ESTADO Inicial dos leds
+		GPIO_Write_Pin(GPIOA, PIN_6, HIGH); // inicia desligado
+
+//		if (GPIO_Read_Pin(GPIOE, PIN_4) == LOW ) { // k0 quando pressionado guarda 1
+//			k0_apertado = 1;
+//		}
+//		else{
+//			k0_apertado = 0;
+//		}
+//		if (GPIO_Read_Pin(GPIOE, PIN_3) == LOW ) { // k1 quando pressionado guarda 1
+//			k1_apertado = 1;
+//		}
+//		else{
+//			k0_apertado = 1;
+//		}
+
+		if (!GPIO_Read_Pin(GPIOE, PIN_4) == 1) {
+			GPIO_Write_Pin(GPIOA, PIN_6, HIGH);
+			if (GPIO_Read_Pin(GPIOE, PIN_3) == LOW){ // quando k0 e k1 forem pressionados
 				GPIO_Write_Pin(GPIOA, PIN_6, LOW);
-				GPIO_Write_Pin(GPIOA, PIN_6, HIGH);
 			}
 		}
+//		if (k1_apertado == 1) {
+//			k0_apertado = 0;
+//			GPIO_Write_Pin(GPIOA, PIN_6, HIGH);
+//			if (k0_apertado == 1){
+//				GPIO_Write_Pin(GPIOA, PIN_6, HIGH);
+//			}
+//		}
+	}
 }
 
+
 int main(void) {
-	questao5();
+	questao9();
 };
 
 
